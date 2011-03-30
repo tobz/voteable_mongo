a3474c3dda8592cd531129c33d3086a6ac2b3304
Alex N <alex@vinova.sg>
2011-03-30 16:58:36 +0800

    Add railtie

--- lib/voteable_mongoid.rb
+++ lib/voteable_mongoid.rb
@@ -2,3 +2,7 @@ require 'voteable_mongoid/voteable/stats'
 require 'voteable_mongoid/voteable'
 require 'voteable_mongoid/voter'
 
+# add railtie
+if defined?(Rails)
+  require 'voteable_mongoid/railtie'
+end
