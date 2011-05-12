d97ece84d3eff54b22e9d3c572bbce3cd4a93b23
Alex N <alex@vinova.sg>
2011-05-13 06:48:39 +0800

    Add helpers

---
+++ lib/voteable_mongo/helpers.rb
@@ -0,0 +1,15 @@
+module Mongo
+  module Voteable
+    module Helpers
+
+      def self.try_to_convert_string_to_object_id(x)
+        x.is_a?(String) && BSON::ObjectId.legal?(x) ? BSON::ObjectId(x) : x
+      end
+      
+      def self.get_mongo_id(x)
+        x.respond_to?(:id) ? x.id : x
+      end
+
+    end
+  end
+end
