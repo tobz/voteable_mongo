c8b2a957dd9183528edcdf7f2186de7ee8bbcea6
Alex N <alex@vinova.sg>
2011-05-13 07:32:34 +0800

    No need rescue

--- lib/voteable_mongo/helpers.rb
+++ lib/voteable_mongo/helpers.rb
@@ -3,11 +3,7 @@ module Mongo
     module Helpers
 
       def self.try_to_convert_string_to_object_id(x)
-        begin
-          x.is_a?(String) && BSON::ObjectId.legal?(x) ? BSON::ObjectId(x) : x
-        rescue
-          x
-        end
+        x.is_a?(String) && BSON::ObjectId.legal?(x) ? BSON::ObjectId(x) : x
       end
 
       def self.get_mongo_id(x)
